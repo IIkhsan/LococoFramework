6d865e914e6f14a651de580e046ab9889bf98ff2
Ilyas Ikhsanov <ilias.ihsanov@gmail.com>
2018-07-10 15:18:15 +0300

    Init

---
+++ LococoFramework/LococoFramework/LococoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LococoFramework.h
+//  LococoFramework
+//
+//  Created by Ilyas on 10.07.2018.
+//  Copyright © 2018 itis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LococoFramework.
+FOUNDATION_EXPORT double LococoFrameworkVersionNumber;
+
+//! Project version string for LococoFramework.
+FOUNDATION_EXPORT const unsigned char LococoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LococoFramework/PublicHeader.h>
+
+
---
+++ LococoFramework/LococoFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  LococoFramework
+//
+//  Created by Ilyas on 10.07.2018.
+//  Copyright © 2018 itis. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {
+        
+    }
+    
+    public static func doSomething() -> String {
+        return "Eeeee"
+    }
+}
